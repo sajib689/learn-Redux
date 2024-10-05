58784197a34cc481f91c108ab0bb1b419b51d0cf
sajib689 <jibonraja11@gmail.com>
2024-10-05 21:51:41 +0600

    increment add an variable

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const INCREMENT = 'INCREMENT'
+
 // state declarations
 const InitialCounterState = {
     count: 0,
@@ -11,4 +13,10 @@ const InitialUsersState = {
 
 // actions|| 2 things have one type and another payload
 // Increment the counter
+const IncrementCounter = () => {
+    return {
+        type: INCREMENT,
+    }
+}
 // Decrement the counter
+
